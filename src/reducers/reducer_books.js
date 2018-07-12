b8e314673864a7be3245dc77c7c5a441955ad190
Sergio <sergiogarher@gmail.com>
2018-07-12 20:59:24 +0200

    Create BooksReducer + Wire it into the application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return[
+        {title: 'Javascript: Ouyeah'},
+        {title: 'Harry the Man'},
+        {title: 'The White Tower'},
+        {title: 'Eloquent Rails'},
+    ]
+}
