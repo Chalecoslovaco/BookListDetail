1f9ebc857722fd1fd5fabf4c4799e7e7c30c3a34
Sergio <sergiogarher@gmail.com>
2018-07-14 13:07:19 +0200

    Add pages property to book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return[
-        {title: 'Javascript: Ouyeah'},
-        {title: 'Harry the Man'},
-        {title: 'The White Tower'},
-        {title: 'Eloquent Rails'},
+        {title: 'Javascript: Ouyeah', pages: 202},
+        {title: 'Harry the Man', pages: 99},
+        {title: 'The White Tower', pages: 555},
+        {title: 'Eloquent Rails', pages: 1},
     ]
 }
